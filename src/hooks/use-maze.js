705bd39774eb0f44046e74f4d9d5535e472d3346
jvdub <jtvanwage@gmail.com>
2022-11-04 20:54:48 -0600

    Update use-maze.js
    Adding notes to the maze path generation method

--- src/hooks/use-maze.js
+++ src/hooks/use-maze.js
@@ -22,46 +22,58 @@ const useMaze = (height, width) => {
   };
   
   const generateMazePath = (maze, currentPosition) => {
-    visited[currentPosition] = true;
-    const node = maze[currentPosition];
-    const neighbors = [...node.potentialEdges];
+    // (the following variable is initialized in a higher scope to an emtpy object)
+    visited[currentPosition] = true; // Mark the current node as visited
+    const node = maze[currentPosition]; // Get a reference to the node
+    const neighbors = [...node.potentialEdges]; // Copy all potential edges (all neighbors)
   
-    while (neighbors.length > 0) {
-      const ki = RandomEdge(neighbors);
+    while (neighbors.length > 0) { // While there are neighbors remaining to visit
+      const nextPosition = RandomEdge(neighbors); // Choose a neighbor to visit at random
   
-      if (!(ki in visited)) {
-        const adjacentNode = maze[ki];
-        node.edges.push(ki);
-        adjacentNode.edges.push(currentPosition);
-        generateMazePath(maze, ki);
+      if (!(nextPosition in visited)) { // If we haven't visited the chosen neighbor
+        const adjacentNode = maze[nextPosition]; // Get a reference to that node
+        node.edges.push(nextPosition); // Add a connection to the chosen node from the current node
+        adjacentNode.edges.push(currentPosition); // Add a connection to the current node from the chosen node
+        generateMazePath(maze, nextPosition); // Move to the chosen node and repeat the process
       }
     }
   
-    return maze;
+    return maze; // When all nodes are visited, return the updated objects
+    // Because JS is pass by reference for non-primitive types, the updates we make to the nodes
+    // in the code above are automatically reflected in the maze object itself.
   };
   
   const generateMaze = (height, width) => {
-    let maze = {};
+    let maze = {}; // Initialize an empty object to build the maze in
   
+    // Loop over the entire height and width
     for (let y = 0; y < width; y++) {
       for (let x = 0; x < height; x++) {
-        maze[_Position(x, y)] = {id: `${x}.${y}`, position: [x, y], weight: 0, edges: [], potentialEdges: [], bordersNeeded: {}};
+        // Create an object with a unique key in the maze object and store things of interest.
+        // Mostly care about edges and potential edges, but can store othes things as needed.
+        maze[_Position(x, y)] = {id: `${x}.${y}`, position: [x, y], weight: 0, edges: [], potentialEdges: []};
       }
     }
   
+    // Loop over the whole maze again to get potential edges
     for (let y = 0; y < width; y++) {
       for (let x = 0; x < height; x++) {
+        // Helper for the current node
         let p = _Position(x, y);
   
         for (let x2 = -1; x2 <= 1; x2++) {
           for (let y2 = -1; y2 <= 1; y2++) {
+            // A little math to make sure we're only moving left, right, up, or down
             if ((x2 === 0 && y2 === 0) || (Math.abs(x2) + Math.abs(y2) !== 1)) {
               continue;
             }
   
+            // Helper for the potential neighbor
             let p2 = _Position(x + x2, y + y2);
   
+            // If it is a node in the maze...
             if (p2 in maze) {
+              // It's a valid potential edge and ew can add it
               maze[p].potentialEdges.push(p2);
             }
           }
@@ -69,9 +81,10 @@ const useMaze = (height, width) => {
       }
     }
   
+    // We'll get to this in a moment. This method generates the path in the maze.
     maze = generateMazePath(maze, _Position(0, 0));
   
-    return maze;
+    return maze; // Return the generated maze
   };
 
   return generateMaze(height, width);
